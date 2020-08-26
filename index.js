a90140f1c30ccbde51f8d1f2ed2667521b1ec66d
rwilt <rosie.wilt@gmail.com>
2020-08-26 17:27:51 -0400

    pseudocode

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//take an array of numbers
+//add every possible combination of these numbers
+//starting from the number at the first index
+//if the next value is greater than the current value, update it
+//return the largest sum 
+
+function largestSubarraySum(array){
+    // code to write here
+   }
