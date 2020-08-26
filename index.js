6837b811e621bc4b5c14c8f815e43184bd7bb12b
rwilt <rosie.wilt@gmail.com>
2020-08-26 17:32:56 -0400

    pseudocode

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
-//take an array of numbers
+//take in an array of numbers
 //add every possible combination of these numbers
-//starting from the number at the first index
+//starting from the number at the first index, and moving on from there.
+//in the example below, it would be 1 + -1, then 1 + -1 + 5, , etc etc. 
 //if the next value is greater than the current value, update it
 //return the largest sum 
 
+let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
+
 function largestSubarraySum(array){
+    const currLargestSum = 0
+    const testArr = []
     // code to write here
    }
